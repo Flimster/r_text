6bb5a2a9a98e5999a0354fc9d1c77de8238fff51
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2020-02-16 12:09:07 +0000

    Added alterantive screen

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ extern crate termion;
 
 use termion::event::Key;
 use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+use termion::raw::{IntoRawMode, RawTerminal};
 use termion::terminal_size;
+use termion::screen::AlternateScreen;
 
 use std::fs::File;
 use std::io::{Write, stdout, stdin, Stdout};
@@ -12,7 +13,6 @@ use std::convert::TryInto;
 // TODO: Keep file in memory, in vector, memory mapped files
 // TODO: Scroll
 // TODO: Change cursor based on mode
-// TODO: Alternative screen
 // TODO: Handle wrapping for very long lines
 
 fn write_to_file(content: Vec<Vec<u8>>) {
@@ -26,22 +26,18 @@ fn write_to_file(content: Vec<Vec<u8>>) {
 fn main() {
     let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
+    let mut screen = AlternateScreen::from(stdout);
 
-    write!(stdout, "{}{}",
+    write!(screen, "{}{}",
            termion::clear::All,
            termion::cursor::Goto(1, 1)).unwrap();
-    stdout.flush().unwrap();
+    screen.flush().unwrap();
 
     let mut x: usize = 1;
     let mut y: usize = 1;
     let (horizontal, vertical) = terminal_size().unwrap();
     let mut a: Vec<Vec<u8>> = Vec::new();
     a.push(Vec::new());
-
-    write!(stdout, "{}{}",
-           termion::clear::All,
-           termion::cursor::Goto(1, 1)).unwrap();
-    stdout.flush().unwrap();
 
     for c in stdin.keys() {
         match c.unwrap() {
@@ -73,6 +69,7 @@ fn main() {
             }
             Key::Char(c) => {
                 if c == '\n' {
+                    // TODO: Based on cursor, insert new line and move text after below
                     match a[y-1].last() {
                         Some(c) if *c != '\n' as u8 => {
                             a[y-1].push('\n' as u8);
@@ -110,30 +107,30 @@ fn main() {
                     a[y - 1].remove(x - 1);
                 }
                 x -= 1;
-                write!(stdout, "{}", termion::clear::CurrentLine).unwrap();
+                write!(screen, "{}", termion::clear::CurrentLine).unwrap();
             }
             _ => {}
         }
-        debug_message(&mut stdout, format!("a={:?}", a));
-        draw_to_screen(&mut stdout, a.clone(), x, y);
+        debug_message(&mut screen, format!("a={:?}", a));
+        draw_to_screen(&mut screen, a.clone(), x, y);
     }
 
-    write!(stdout, "{}", termion::cursor::Show).unwrap();
+    write!(screen, "{}", termion::cursor::Show).unwrap();
 }
 
 
-fn draw_to_screen(mut stdout: &mut Stdout, lines: Vec<Vec<u8>>, x: usize, y: usize) {
+fn draw_to_screen(mut screen: &mut AlternateScreen<RawTerminal<Stdout>>, lines: Vec<Vec<u8>>, x: usize, y: usize) {
     let mut count = 1;
     for item in lines {
         let mut b = String::from_utf8(item.clone()).unwrap();
         if item == [0xa] {
-            write!(stdout,
+            write!(screen,
                    "{}{}{}",
                    termion::cursor::Goto(1, count),
                    termion::clear::CurrentLine,
                    termion::cursor::Goto(x as u16, y as u16)).unwrap();
         } else {
-            write!(stdout,
+            write!(screen,
                    "{}{}{}",
                    termion::cursor::Goto(1, count),
                    b,
@@ -141,10 +138,10 @@ fn draw_to_screen(mut stdout: &mut Stdout, lines: Vec<Vec<u8>>, x: usize, y: usi
         }
         count += 1;
     }
-    stdout.flush().unwrap();
+    screen.flush().unwrap();
 }
 
-fn debug_message(stdout: &mut Stdout, debug_message: String) {
-    write!(stdout, "{}{}{}", termion::cursor::Goto(50, 50), termion::clear::CurrentLine, debug_message).unwrap();
-    stdout.flush().unwrap();
+fn debug_message(screen: &mut AlternateScreen<RawTerminal<Stdout>>, debug_message: String) {
+    write!(screen, "{}{}{}", termion::cursor::Goto(50, 50), termion::clear::CurrentLine, debug_message).unwrap();
+    screen.flush().unwrap();
 }
